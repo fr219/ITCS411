a8e73e571b7565ad0f4e6b78a924ee68acd0a005
fatema09 <123894379+fatema09@users.noreply.github.com>
2025-10-20 09:16:26 +0300

    Tutorial_2

--- my_first_project/my_first_program.py
+++ my_first_project/my_first_program.py
@@ -24,6 +24,48 @@ print_hello()
 print_greeting("John")
 
 if my_age > 18:
-    print("You are an adult.")
+    print("You are an adult.\n")
 else:
-    print("You are a minor.")
+    print("You are a minor.\n")
+
+#Lists are used to store multiple values in a single variable. 
+# Each value in a list is separated by a comma and enclosed in square brackets. Lists can contain values of different data types.
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(fruits[1])
+print(len(fruits))
+
+fruits.append("orange")
+print(fruits)
+
+fruits.remove("banana")
+print(fruits,"\n")
+
+#Loops are used to execute a block of code repeatedly. There are two types of loops in Python: for loops and while loops.
+for fruit in fruits:
+    print(fruit)
+
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
+print("\n")
+
+#Dictionaries are used to store key-value pairs. Each key in a dictionary is associated with a value, 
+# and the key-value pairs are separated by colons and enclosed in curly braces.
+student = {
+    "name": "John",
+    "age": 30,
+    "is_student": True
+}
+
+print(student)
+print(student["name"])
+print(len(student))
+
+student["age"] = 31
+print(student)
+
+del student["is_student"]
+print(student,"\n")
+
